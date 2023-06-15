9c99273664edb0088d89ec0b94e9e75ec3d1cee9
Anthony Pérez Meza <136654564+AnthonyProf@users.noreply.github.com>
2023-06-14 18:00:56 -0600

    Tercer commit

--- 3CAPAS-Update/src/pkg3capas/update/Update.java
+++ 3CAPAS-Update/src/pkg3capas/update/Update.java
@@ -14,7 +14,11 @@ public class Update {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        
+        System.out.println("Hola a todos");
+        System.out.println("Prueba 1");
+        
+        
     }
     
 }
